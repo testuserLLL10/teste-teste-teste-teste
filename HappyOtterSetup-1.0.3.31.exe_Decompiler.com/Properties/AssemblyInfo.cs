ec81ae78f38de22711908c356394eaa5b615dbe4
testuserLLL10 <tanjirotanjirokamado77@gmail.com>
2024-06-18 22:04:22 -0300

    Add files via upload

---
+++ HappyOtterSetup-1.0.3.31.exe_Decompiler.com/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyTitle("HappyOtterSetup")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyFileVersion("1.0.3.31")]
+[assembly: Dotfuscator("252806:0:0:6.3.0+8b29d6f4a2", 0)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HappyOtterSetup")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("60a4eb4a-f598-4a47-ac46-05579654e437")]
+[assembly: AssemblyVersion("1.0.3.31")]
